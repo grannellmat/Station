ad2a8b6e8cdd2799286a5ae64b7b7ddb387384ff
grannellmat <grannellmat@gmail.com>
2017-07-31 09:40:08 +0100

    Add message component and navigation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,12 @@ class WelcomeController < ApplicationController
   def index
     @hello = 'Hello'
   end
+
+  def about
+    @hello = 'Hello'
+  end
+
+  def contact
+    @hello = 'Hello'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/about', to: 'welcome#about', as: 'about'
+  get '/contact', to: 'welcome#contact', as: 'contact'
 
 
   # Example of regular route:
